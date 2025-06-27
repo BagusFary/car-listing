8a3cffa6896a5278e6e2af3d07feff7c4367668f
BagusFary <bagusfaryananta@gmail.com>
2025-06-27 17:59:14 +0700

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+export const App = () => {
+    return (
+        <>
+            <div className="h-screen w-screen flex justify-center items-center">
+                <h1 className="text-5xl font-mono font-semibold">Hello World</h1>
+            </div>
+        </>
+    )
+}
