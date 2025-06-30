654f4dfe6b18bef075ab260356b3840171388e30
BagusFary <bagusfaryananta@gmail.com>
2025-06-30 09:48:21 +0700

    commit-from-laptop

--- server/index.js
+++ server/index.js
@@ -4,11 +4,20 @@ const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+app.get('/product/:id', () => {
+    const productId = req.params.id;
+    res.send(req.params);
+    // res.send(`ID Product yang dicari adalah ${productId}`);
+    // console.log(`ID Product yang dicari adalah ${productId}`);
 });
 
+app.post('/products', (req, res, next) => {
+    console.log(`Product dan Page akan di proses di callback ke 2`);
+    next();
+}, (req, res) => {
+    const product = req.query.keyword;
+    const page = req.query.page;
+    res.send(`Product yang dicari adalah ${product}, halaman yang diinginkan adalah ${page}`);
+});
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-})
+app.listen(port);
